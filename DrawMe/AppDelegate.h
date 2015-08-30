0aa05b71dbaea8012abf2688da256c91f8d62474
JimSRush <jim@galesburg.co.nz>
2015-08-31 10:19:06 +1200

    Initial Commit

---
+++ DrawMe/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DrawMe
+//
+//  Created by Jim Rush on 31/08/15.
+//  Copyright (c) 2015 Jim Rush. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DrawMe/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DrawMe
+//
+//  Created by Jim Rush on 31/08/15.
+//  Copyright (c) 2015 Jim Rush. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
