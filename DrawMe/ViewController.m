85bd65e3866ec8ae97d44641b824975ce8b22b17
JimSRush <jim@galesburg.co.nz>
2015-09-01 11:15:31 +1200

    Added eraser.

--- DrawMe/ViewController.m
+++ DrawMe/ViewController.m
@@ -14,14 +14,89 @@
 
 @implementation ViewController
 
+/*Initialise variables to something non-stupid*/
 - (void)viewDidLoad {
+    
+    red = 0.0/255.0;
+    green = 0.0/255.0;
+    blue = 0.0/255.0;
+    brush = 10.0;
+    opacity = 1.0;
+    
+    
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
+
+
+/*This is the method that responds to a new touch event*/
+-(void) touchesBegan:(NSSet *)touches withEvent: (UIEvent *)event{
+    mouseSwiped = NO;
+    UITouch *touch = [touches anyObject]; //register the touch and save it
+    lastPoint = [touch locationInView:self.view]; //where did we last touch?
+}
+
+
+/*Get the current touch point, and draw a line from the lastPoint to currentPoint
+ The lines are very short, so it makes a good curve*/
+-(void)touchesMoved:(NSSet *) touches withEvent:(UIEvent *)event{
+    mouseSwiped = YES;//Ok, we're doing it
+    UITouch *touch = [touches anyObject];
+    CGPoint currentPoint = [touch locationInView:self.view];
+    
+    UIGraphicsBeginImageContext(self.view.frame.size);//This is what we're working on
+    [self.tempDrawImage.image drawInRect:CGRectMake(0,0, self.view.frame.size.width, self.view.frame.size.height)];
+    CGContextMoveToPoint(UIGraphicsGetCurrentContext(), lastPoint.x, lastPoint.y);
+    CGContextAddLineToPoint(UIGraphicsGetCurrentContext(), currentPoint.x, currentPoint.y);//add line
+    CGContextSetLineCap(UIGraphicsGetCurrentContext(), kCGLineCapRound);
+    CGContextSetLineWidth(UIGraphicsGetCurrentContext(), brush);//how wide?
+    CGContextSetRGBStrokeColor(UIGraphicsGetCurrentContext(), red, green, blue, 1.0);//what colour?
+    CGContextSetBlendMode(UIGraphicsGetCurrentContext(), kCGBlendModeNormal);
+    
+    CGContextStrokePath(UIGraphicsGetCurrentContext());//we're doing itttttt (drawing the path)
+    self.tempDrawImage.image = UIGraphicsGetImageFromCurrentImageContext();
+    [self.tempDrawImage setAlpha:opacity];
+    UIGraphicsEndImageContext();
+    
+    lastPoint = currentPoint;
+    
+}
+
+
+/*If it's swiped, forgeddaboutit. If not, we're drawing a dot. */
+-(void) touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event {
+    if(!mouseSwiped){
+        UIGraphicsBeginImageContext(self.view.frame.size);
+        [self.tempDrawImage.image drawInRect:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height)];
+        CGContextSetLineCap(UIGraphicsGetCurrentContext(), kCGLineCapRound);
+        CGContextSetLineWidth(UIGraphicsGetCurrentContext(), brush);
+        CGContextSetRGBStrokeColor(UIGraphicsGetCurrentContext(), red, green, blue, opacity);
+        CGContextMoveToPoint(UIGraphicsGetCurrentContext(), lastPoint.x, lastPoint.y);
+        CGContextAddLineToPoint(UIGraphicsGetCurrentContext(), lastPoint.x, lastPoint.y);
+        CGContextStrokePath(UIGraphicsGetCurrentContext());
+        CGContextFlush(UIGraphicsGetCurrentContext());
+        self.tempDrawImage.image = UIGraphicsGetImageFromCurrentImageContext();
+        UIGraphicsEndImageContext();
+    }
+    
+    UIGraphicsBeginImageContext(self.mainImage.frame.size);
+    [self.mainImage.image drawInRect:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height) blendMode:kCGBlendModeNormal alpha:1.0];
+    [self.tempDrawImage.image drawInRect:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height) blendMode:kCGBlendModeNormal alpha:opacity];
+    self.mainImage.image = UIGraphicsGetImageFromCurrentImageContext();
+    self.tempDrawImage.image = nil;
+    UIGraphicsEndImageContext();
+    
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)pencilPressed:(id)sender {
+}
+
+- (IBAction)eraserPressed:(id)sender {
+}
 @end
